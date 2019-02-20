92402d6f621908a321d6b1a6c0cf8d9fec1cb4d7
Lucas Zacharewicz <lucas.zacharewicz@gmail.com>
2019-02-19 21:03:52 -0500

    Added gameloop to mainfile

--- Food/src/__main__.py
+++ Food/src/__main__.py
@@ -1,8 +1,34 @@
+## @file   __main__.py
+#  @title  Main game file
+#  @author Lucas Zacharewicz
+#  @date   February 19, 2019
+
+###########
+# Imports #
+###########
+
+import pygame
+from gameLoop.gameloop import *
+
+
 ## @brief  Entering point of the game.
 #  @detail Creates game window object and handles pygame.quit
 def main():
+    
+    # Initialize pygame
+    pygame.init()
+    
+    # Initialize game loop
+    game = GameLoop()
 
+    # Run game
+    game.run()
 
+    # IO output
+    print("Exited game successfully")
+
+    # Quit pygame
+    pygame.quit()
 
 if __name__=="__main__":
     main()
